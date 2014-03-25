5cf7a64a7ca5ecc199c695cac4f9e2719080fd5a
Huw Giddens <hgiddens@gmail.com>
2014-03-25 21:48:20 +1100

    Exercises completed.

--- ch01/WC.hs
+++ ch01/WC.hs
@@ -1,5 +1,11 @@
 -- file: ch01/WC.hs
 -- lines beginning with "--" are comments.
 
-main = interact wordCount
-    where wordCount input = show (length (lines input)) ++ "\n"
+main = interact wordCount''
+    where
+      -- lines
+      wordCount input = show (length (lines input)) ++ "\n"
+      -- words
+      wordCount' input = ((show . length . words) input) ++ "\n"
+      -- characters
+      wordCount'' input = ((show . length) input) ++ "\n"
